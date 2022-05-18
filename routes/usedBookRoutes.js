8fe400b2eca25ce3bb5848ec213db1f1c74feacb
tonyxuan2021 <tonyyexuan@gmail.com>
2022-05-18 10:28:18 -0700

    complete post request not tested yet

--- routes/usedBookRoutes.js
+++ routes/usedBookRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
+var uniqid = require('uniqid'); 
+
+
 
 router.use((req, res, next) => {
     console.log('Notes Router Incoming Request');
@@ -12,9 +15,34 @@ router.use((req, res, next) => {
       return JSON.parse(booksContent)
   }
 
+  const writeBook = (data) => {
+    const stringifiedData = JSON.stringify(data);
+    fs.writeFileSync("./data/books.json", stringifiedData)
+  }
+
   router.get("/usedbook", (req,res)=> {
       const booksContent = readBook();
       res.status(200).json(booksContent)
   })
+
+  router.post("/usedbook", (req,res)=> {
+      const booksContent = readBook();
+
+      const newBook = {
+        id:uniqid(),
+        name: req.body.name,
+        price: req.body.price
+      }
+
+      booksContent.push(newBook);
+
+      writeBook(booksContent);
+
+      res.status(201).json(newBook);
+  })
+
+
+
+
 
   module.exports = router;
