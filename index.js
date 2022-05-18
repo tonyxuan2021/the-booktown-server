9729e8674c4f1b588cf43abb7a1df6e2ad194c98
tonyxuan2021 <tonyyexuan@gmail.com>
2022-05-18 15:29:04 -0700

    fix gitignore

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 let uniqid = require("uniqid");
+const fileUpload = require("express-fileupload");
+
 const paymentRoute = require("./routes/paymentRoutes")
 const usedBookRoute = require("./routes/usedBookRoutes")
 
@@ -14,10 +16,11 @@ require("dotenv").config();
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cors());
+app.use(fileUpload());
 
 
-app.use('/',paymentRoute);
-app.use('/',usedBookRoute);
+app.use('/payment',paymentRoute);
+app.use('/usedbook',usedBookRoute);
 
 
 // Listen
--- routes/usedBookRoutes.js
+++ routes/usedBookRoutes.js
@@ -1,48 +1,48 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fs = require('fs');
-var uniqid = require('uniqid'); 
-
-
+const fs = require("fs");
+var uniqid = require("uniqid");
 
 router.use((req, res, next) => {
-    console.log('Notes Router Incoming Request');
-    next();
-  });
+  console.log("Notes Router Incoming Request");
+  next();
+});
 
-  const readBook = () => {
-      const booksContent = fs.readFileSync("./data/books.json");
-      return JSON.parse(booksContent)
-  }
+const readBook = () => {
+  const booksContent = fs.readFileSync("./data/books.json");
+  return JSON.parse(booksContent);
+};
 
-  const writeBook = (data) => {
-    const stringifiedData = JSON.stringify(data);
-    fs.writeFileSync("./data/books.json", stringifiedData)
-  }
+const writeBook = (data) => {
+  const stringifiedData = JSON.stringify(data);
+  fs.writeFileSync("./data/books.json", stringifiedData);
+};
 
-  router.get("/usedbook", (req,res)=> {
-      const booksContent = readBook();
-      res.status(200).json(booksContent)
-  })
+router.get("/", (req, res) => {
+  const booksContent = readBook();
+  res.status(200).json(booksContent);
+});
 
-  router.post("/usedbook", (req,res)=> {
-      const booksContent = readBook();
+router.post("/", (req, res) => {
+  const booksContent = readBook();
 
-      const newBook = {
-        id:uniqid(),
-        name: req.body.name,
-        price: req.body.price
-      }
+  const newBook = {
+    id: uniqid(),
+    name: req.body.name,
+    price: req.body.price,
+    filepathUrl: `http://localhost:5050/${req.body.filepathUrl}`
+  };
 
-      booksContent.push(newBook);
+  booksContent.unshift(newBook);
 
-      writeBook(booksContent);
+  writeBook(booksContent);
 
-      res.status(201).json(newBook);
-  })
+  res.status(201).json(newBook);
+});
 
+router.post("/image", (req, res) => {
+  console.log(req.files);
+  res.status(201).send("test done")
+});
 
-
-
-
-  module.exports = router;
+module.exports = router;
