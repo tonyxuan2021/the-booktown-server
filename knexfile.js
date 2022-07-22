225e2336686b412640a7cb5cb6ad9a160dc796d3
tonyxuan2021 <tonyyexuan@gmail.com>
2022-07-22 09:04:52 -0700

    fix backend

--- knexfile.js
+++ knexfile.js
@@ -1,28 +1,21 @@
 // Update with your config settings.
 
-// require("dotenv").config();
+require("dotenv").config();
+
+
 
 /**
-//  * @type { Object.<string, import("knex").Knex.Config> }
+ * @type { Object.<string, import("knex").Knex.Config> }
  */
-const connections = {
+ module.exports = {
   development: {
-    client: "mysql",
+    client: 'mysql',
     connection: {
-      host: "127.0.0.1",
-      user: "root",
+      host: '127.0.0.1',
+      user: 'root',
       password: process.env.KNEXPASSWORD,
-      database: "booktown_cda",
-      charset: "utf8",
+      database: 'booktown_cda',
+      charset: 'utf8',
     },
   },
-  production: {
-    client: "mysql",
-    connection: process.env.JAWSDB_URL,
-  },
 };
-
-module.exports =
-  process.env.NODE_ENV === "production"
-    ? connections.production
-    : connections.development;
